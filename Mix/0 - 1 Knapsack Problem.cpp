1482ff4e21cdb1bf62c65cb7aa0cfd3c74fe6065
Vandana Punjabi <94670086+VandanaPunjabi@users.noreply.github.com>
2023-07-13 16:03:35 +0530

    Create 0 - 1 Knapsack Problem.cpp

---
+++ Mix/0 - 1 Knapsack Problem.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       
+       vector<int>ans(W+1,0);
+       
+       for(int i=1;i<=n;i++){
+           
+           for(int j=W; j>=wt[i-1]; j--){
+               
+               ans[j] = max(val[i-1] + ans[j-wt[i-1]], ans[j]);
+           }
+       }
+       
+       return ans[W];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
